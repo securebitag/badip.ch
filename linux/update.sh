5213fb6d025e204810515fb9a102dd30b1220117
Securebit AG <45911247+securebitag@users.noreply.github.com>
2018-12-23 16:45:29 +0100

    Create update.sh

---
+++ linux/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#--------------------------------------------------------------------#
+# Copyright 2018 by Kevin Buehl <kevin.buehl@securebit.cloud>        #
+#--------------------------------------------------------------------#
+#     _____                          _     _ _              _____    #
+#    / ____|                        | |   (_) |       /\   / ____|   #
+#   | (___   ___  ___ _   _ _ __ ___| |__  _| |_     /  \ | |  __    #
+#    \___ \ / _ \/ __| | | | '__/ _ \ '_ \| | __|   / /\ \| | |_ |   #
+#    ____) |  __/ (__| |_| | | |  __/ |_) | | |_   / ____ \ |__| |   #
+#   |_____/ \___|\___|\__,_|_|  \___|_.__/|_|\__| /_/    \_\_____|   #
+#                                                                    #
+# No part of this script or any of its contents may be reproduced,   #
+# copied, modified or adapted, without the prior written consent of  #
+# the author, unless otherwise indicated for stand-alone materials.  #
+# For more Information visit www.securebit.cloud.                    #
+# This notice must remain untouched at any time.                     #
+#--------------------------------------------------------------------#
