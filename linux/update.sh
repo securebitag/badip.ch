5a8bb60b5c608ae432ae61b912f3330e5b587b5c
Securebit AG <45911247+securebitag@users.noreply.github.com>
2018-12-23 18:47:47 +0100

    Update update.sh

--- linux/update.sh
+++ linux/update.sh
@@ -15,3 +15,39 @@
 # For more Information visit www.securebit.cloud.                    #
 # This notice must remain untouched at any time.                     #
 #--------------------------------------------------------------------#
+DSTDIR="/opt/badip"
+
+# check if apikey file exist
+if ! [ -f "${DSTDIR}/api.key" ]; then
+   echo "Please use our install script." 1>&2
+   exit 1
+else
+   APIKEY=$(cat ${DSTDIR}/api.key)
+fi
+# check if script run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# check if fail2ban installed
+if [ ! -d "/etc/fail2ban/" ]; then
+   echo "please install fail2ban"
+   exit 1
+elif [ ! -e "/etc/fail2ban/ip.blacklist" ]; then
+   touch /etc/fail2ban/ip.blacklist
+fi
+# check if apikey not empty
+if [ -z "$APIKEY" ]; then
+   echo "Please enter your APIKEY in file /opt/4b42/api.key"
+fi
+# files
+IPS=$(cat /etc/fail2ban/black.list)
+status=`wget --header="APIKEY:${APIKEY}" --post-data="ips=$IPS" -O /tmp/black.list https://api.badip.ch --no-check-certificate 2>&1|egrep "HTTP"|awk {'print $6'}`
+if [ "$status" == 200 ]; then
+   rm -f /etc/fail2ban/black.list
+   mv /tmp/black.list /etc/fail2ban/black.list
+elif [ -e "/tmp/black.list" ]; then
+   echo $(cat /tmp/black.list);
+fi
+# reload fail2ban service
+service fail2ban reload
